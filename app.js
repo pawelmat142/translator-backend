4b5977baa17da148d6a804da2e8cb6ad31bfc6e5
pawelmat142 <pawelmat142@gmail.com>
2022-07-17 14:29:51 +0200

    port changes to work with proxy server

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.use(express.static(path.join(__dirname, '/public')))
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = 8080
+const port = 8003
 app.listen(port, () => console.log('to-do list app listening on port: ' + port))
